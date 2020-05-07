9d709dc37ad5e683aa542367101b98d2e02f7525
jonathantsang <j26tsang@gmail.com>
2020-05-06 22:27:51 -0700

    cf c639

--- codeforces/contest639/b/b.py
+++ codeforces/contest639/b/b.py
@@ -1,4 +1,29 @@
 t = int(input())
 
+def solve(n):
+	total = 0
+	# largest size first
+	sizes = [2]
+	amt = 2
+	triangles = 1
+	while amt <= n:
+		# +3 each time
+		# +3 for each triangle
+		# +2 for last triangle
+		amt += (triangles-1)*3 + 2 + 3
+		triangles += 1
+		sizes.append(amt)
+	#print(sizes)
+
+	idx = len(sizes)-1
+	while idx >= 0:
+		if sizes[idx] > n:
+			idx -= 1
+		else:
+			n -= sizes[idx]
+			total += 1
+	return total
+
 for _ in range(t):
-	pass
+	n = int(input())
+	print(solve(n))
--- codeforces/contest639/c/c.py
+++ codeforces/contest639/c/c.py
@@ -1,4 +1,15 @@
 t = int(input())
 
+def solve(n, arr):
+	rooms = set() # room #
+	for i, v in enumerate(arr):
+		newroom = i + arr[i % n]
+		if newroom in rooms:
+			return "NO"
+		rooms.add(newroom)
+	return "YES"
+
 for _ in range(t):
-	pass
+	n = int(input())
+	arr = list(map(int, input().split()))
+	print(solve(n, arr))
